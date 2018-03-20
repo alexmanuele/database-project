fb13cc34a750d34eb4f392b87ec493341aa078a4
ajoeman <30836347+ajoeman@users.noreply.github.com>
2018-03-20 17:57:18 -0300

    Connect schedule.php to database

--- schedule.php
+++ schedule.php
@@ -1,19 +1,20 @@
 <?php
-
 if(isset($_POST["sched"]))
 {
-  echo "this worked";
+
   $servername = "db.cs.dal.ca";
   $username = "manuele";
   $password = "B00559291";
   $dbname = "manuele";
 
   $date=$_POST['date'];
-  echo $date;
+  echo "<br>" . $date . "<br>";
+
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
   if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
   }
-
   echo"<table>
     <thead>
       <tr>
@@ -23,21 +24,18 @@ if(isset($_POST["sched"]))
       </tr>
     </thead>
     <tbody>";
-
-  $q = "select * from Schedule where Date = '$date';";
-  $r = $conn->query($q);
-
-  while($rw=mysqli_fetch_assoc($r))
+  $query = "select * from Schedule where Date = '$date';";
+  $result = $conn->query($query);
+  while($row=mysqli_fetch_assoc($result))
   {
-    echo"<tr>
-      <td>$rw[Block_ID]
-      <td>$rw[Class_Code]
+    echo "<tr>
+      <td>$row[Block_ID]
+      <td>$row[Class_Code]
       <td>Teacher_Name_here
       </tr>";
   }
   echo "</tbody>
       </table>";
-
+ $conn->close();
 }
-
  ?>
